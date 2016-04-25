e668d02d03c54a145451b2037dd8981bd0ffd468
Chung <chungman1993@gmail.com>
2016-04-25 15:15:38 +0100

    Grid view styled

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$('.toggle-btn, .close-btn').click(function(){
-	$(this).parent().toggleClass('active');
+$('.options').click(function(){
+	if($('.content-wrapper').hasClass('list-view')){
+		$('.content-wrapper').removeClass('list-view');
+		$('.content-wrapper').addClass('grid-view');
+	} else {
+		$('.content-wrapper').removeClass('grid-view');
+		$('.content-wrapper').addClass('list-view');
+	}
 });
